886a83c4e7e3570b2c2713acbc99896d948ef97c
Konstantin Sonntag <konstantin.sonntag@web.de>
2020-10-09 15:31:35 +0200

    changed function arrayModel() to only use CPLEX

--- src/ModelBuilds.jl
+++ src/ModelBuilds.jl
@@ -18,36 +18,21 @@ function arrayModel(coeff::Array{Float64,2}, bound::Array{Float64,1}, n::Int64, 
 
 	if directMode == false
 		m = Model();
-		if optimizer == CPLEX_opt
-			set_optimizer(m, CPLEX.Optimizer)
-		elseif optimizer == Gurobi_opt
-			set_optimizer(m, Gurobi.Optimizer)	
-		elseif optimizer == Mosek_opt
-			set_optimizer(m, Mosek.Optimizer)
-		elseif optimizer == Xpress_opt
-			set_optimizer(m, Xpress.Optimizer)
-		end
+		set_optimizer(m, CPLEX.Optimizer)
+
 	else
-		if optimizer == CPLEX_opt
-			m = direct_model(CPLEX.Optimizer());
-		elseif optimizer == Gurobi_opt
-			m = direct_model(Gurobi.Optimizer());
-		elseif optimizer == Mosek_opt
-			m = direct_model(Mosek.Optimizer());
-		elseif optimizer == Xpress_opt
-			m = direct_model(Xpress.Optimizer());
-		end
+		m = direct_model(CPLEX.Optimizer());
 	end
 
 	@variable(m,0<= x[1:n]<=1)
 	@constraint(m, con[i = 1:n], sum(coeff[i,j].*x[j] for j = 1:n) <= bound[i] )
-	
+
 	@objective(m, Max, sum(x))
-		
+
 	if optimization == true
 		JuMP.optimize!(m)
 	end
-	
+
 	return(m)
 
 end
@@ -71,14 +56,14 @@ function arrayModelLongName(coeff::Array{Float64,2}, bound::Array{Float64,1}, n:
 
 	@variable(m, 0<= verylooooooooongname[1:n]<=1,base_name = "x")
 	@constraint(m, con[i = 1:n], sum(coeff[i,:].*verylooooooooongname) <= bound[i] )
-	
+
 
 	@objective(m, Max, sum(verylooooooooongname))
-		
+
 	if optimization == true
 		JuMP.optimize!(m)
 	end
-	
+
 	return(m)
 
 end
@@ -94,21 +79,21 @@ function arrayModelLongBasename(coeff::Array{Float64,2}, bound::Array{Float64,1}
 
 	if directMode == false
 		m = Model();
-		et_optimizer(m, CPLEX.Optimizer)	
+		et_optimizer(m, CPLEX.Optimizer)
 	else
-		m = direct_model(CPLEX.Optimizer());	
+		m = direct_model(CPLEX.Optimizer());
 	end
 
 	@variable(m, 0<= x[1:n]<=1,base_name = "VERYLOOOOOOOOONGNAME")
 	@constraint(m, con[i = 1:n], sum(coeff[i,:].*x) <= bound[i] )
-	
+
 
 	obj = @objective(m, Max, sum(x))
-		
+
 	if optimization == true
 		JuMP.optimize!(m)
 	end
-	
+
 	return(m)
 
 end
@@ -127,12 +112,12 @@ function denseAxisArrayModel(coeff::JuMP.Containers.DenseAxisArray{Float64,2,Tup
 		m = Model();
 		set_optimizer(m, CPLEX.Optimizer)
 	else
-		m = direct_model(CPLEX.Optimizer());	
+		m = direct_model(CPLEX.Optimizer());
 	end
 
 	@variable(m,0<= x[1:n]<=1, container = DenseAxisArray)
 	@constraint(m, con[i = 1:n], sum(coeff[i,j]*x[j] for j in 1:n) <= bound[i], container = DenseAxisArray)
-	
+
 
 	@objective(m, Max, sum(x[j] for j in 1:n))
 
@@ -177,7 +162,7 @@ end
 function denseAxisArrayModelLongNameShortBasename(coeff::JuMP.Containers.DenseAxisArray{Float64,2,Tuple{UnitRange{Int64},UnitRange{Int64}},Tuple{Dict{Int64,Int64},Dict{Int64,Int64}}},
 	bound::JuMP.Containers.DenseAxisArray{Float64,1,Tuple{UnitRange{Int64}},Tuple{Dict{Int64,Int64}}}, n::Int64, directMode::Bool, optimization::Bool)
 	#DataType DAALS
-	
+
 	#coeff 				matrix: for defintion of the linear constraints of the form coeff*x .<= bound
 	#bound 				array: for definition of constraints (see above)
 	#n 					integer: showing problem size
@@ -186,7 +171,7 @@ function denseAxisArrayModelLongNameShortBasename(coeff::JuMP.Containers.DenseAx
 
 	if directMode == false
 		m = Model();
-		set_optimizer(m, CPLEX.Optimizer)		
+		set_optimizer(m, CPLEX.Optimizer)
 	else
 		m = direct_model(CPLEX.Optimizer());
 	end
@@ -208,7 +193,7 @@ end
 function denseAxisArrayModelLongBasename(coeff::JuMP.Containers.DenseAxisArray{Float64,2,Tuple{UnitRange{Int64},UnitRange{Int64}},Tuple{Dict{Int64,Int64},Dict{Int64,Int64}}},
 	bound::JuMP.Containers.DenseAxisArray{Float64,1,Tuple{UnitRange{Int64}},Tuple{Dict{Int64,Int64}}}, n::Int64, directMode::Bool, optimization::Bool)
 	#DAALB
-	
+
 	#coeff 				matrix: for defintion of the linear constraints of the form coeff*x .<= bound
 	#bound 				array: for definition of constraints (see above)
 	#n 					integer: showing problem size
@@ -217,7 +202,7 @@ function denseAxisArrayModelLongBasename(coeff::JuMP.Containers.DenseAxisArray{F
 
 	if directMode == false
 		m = Model();
-		set_optimizer(m, CPLEX.Optimizer)	
+		set_optimizer(m, CPLEX.Optimizer)
 	else
 		m = direct_model(CPLEX.Optimizer());
 	end
@@ -282,9 +267,9 @@ function dictModel(coeff::Dict{Any,Any}, bound::Dict{Any,Any}, n::Int64, directM
 		m = Model();
 		set_optimizer(m, CPLEX.Optimizer)
 	else
-		m = direct_model(CPLEX.Optimizer());	
+		m = direct_model(CPLEX.Optimizer());
 	end
-	
+
 	x = Dict()
 	for j = 1:n
 		x[j] = @variable(m, lower_bound = 0, upper_bound =1)
@@ -307,7 +292,7 @@ end
 
 function dictModelStr(coeff::Dict{Any,Any}, bound::Dict{Any,Any}, str_obj::Array{String,1}, str_con::Array{String,1}, directMode::Bool, optimization::Bool)
 	#DataType DS
-	
+
 	#coeff 				matrix: for defintion of the linear constraints of the form coeff*x .<= bound
 	#bound 				array: for definition of constraints (see above)
 	#n 					integer: showing problem size
@@ -316,11 +301,11 @@ function dictModelStr(coeff::Dict{Any,Any}, bound::Dict{Any,Any}, str_obj::Array
 
 	if directMode == false
 		m = Model();
-		set_optimizer(m, CPLEX.Optimizer)	
+		set_optimizer(m, CPLEX.Optimizer)
 	else
 		m = direct_model(CPLEX.Optimizer());
 	end
-	
+
 	x = Dict()
 	for name_obj in str_obj
 		x[name_obj] = @variable(m, lower_bound = 0, upper_bound = 1)
@@ -342,7 +327,7 @@ end
 
 function dictionaryModel(coeff::Dictionary{CartesianIndex{2},Float64}, bound::Dictionary{Int64,Float64}, n::Int64, directMode::Bool, optimization::Bool)
 	#DataType DD
-	
+
 	#coeff 				matrix: for defintion of the linear constraints of the form coeff*x .<= bound
 	#bound 				array: for definition of constraints (see above)
 	#n 					integer: showing problem size
@@ -351,11 +336,11 @@ function dictionaryModel(coeff::Dictionary{CartesianIndex{2},Float64}, bound::Di
 
 	if directMode == false
 		m = Model();
-		set_optimizer(m, CPLEX.Optimizer)	
+		set_optimizer(m, CPLEX.Optimizer)
 	else
-		m = direct_model(CPLEX.Optimizer());	
+		m = direct_model(CPLEX.Optimizer());
 	end
-	
+
 	x = Dictionary(1:n,@variable(m, 0 <= x[1:n] <= 1))
 	con = Dictionary(1:n, @constraint(m, con[i = 1:n], sum(coeff[CartesianIndex(i ,j)]*x[j] for j in 1:n) <= bound[i]))
 
@@ -371,20 +356,20 @@ end
 
 function namedDimsModel(coeff::NamedDimsArray{(:x1, :x2),Float64,2,Array{Float64,2}}, bound::NamedDimsArray{(:x1,),Float64,1,Array{Float64,1}}, n::Int64, directMode::Bool, optimization::Bool, optimizer::Optimizers)
 	#DataType ND
-	
+
 	#coeff 				matrix: for defintion of the linear constraints of the form coeff*x .<= bound
 	#bound 				array: for definition of constraints (see above)
 	#n 					integer: showing problem size
 	#directMode 		bool: info for model build
 	#optimization		bool: shall the JuMP model be optimized
-	
+
 	if directMode == false
 		m = Model();
-		set_optimizer(m, CPLEX.Optimizer)	
+		set_optimizer(m, CPLEX.Optimizer)
 	else
-		m = direct_model(CPLEX.Optimizer());	
+		m = direct_model(CPLEX.Optimizer());
 	end
-	
+
 	x = NamedDimsArray{(:x2,)}(@variable(m, 0 <= x[1:n] <= 1))
 
 	con = NamedDimsArray{(:x1,)}(@constraint(m, con[i = 1:n], sum(coeff[x1 = i ,x2 = j]*x[x2 = j] for j in 1:n) <= bound[i] ))
